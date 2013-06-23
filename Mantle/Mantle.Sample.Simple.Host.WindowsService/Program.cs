af3eb5990daa3a2bf55bef7989ba4f038f8e8260
Casey Watson <casey.j.watson@gmail.com>
2013-06-22 19:25:41 -0500

    Added a worker host for Windows Services.

--- Mantle/Mantle.Sample.Simple.Host.WindowsService/Program.cs
+++ Mantle/Mantle.Sample.Simple.Host.WindowsService/Program.cs
@@ -1,8 +1,6 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using Mantle.Hosting;
+﻿using System.Reflection;
+using Mantle.Ninject;
 using Ninject;
-using Topshelf;
 
 namespace Mantle.Samples.Simple.Host.WindowsService
 {
@@ -10,35 +8,13 @@ namespace Mantle.Samples.Simple.Host.WindowsService
     {
         private static void Main(string[] args)
         {
-            HostFactory.Run(x =>
-                {
-                    x.Service<IWorker>(s =>
-                        {
-                            s.ConstructUsing(c => CreateWorker());
-                            s.WhenStarted(c => c.Start());
-                            s.WhenStopped(c => c.Stop());
-                        });
-
-                    x.RunAsLocalSystem();
-
-                    x.SetDescription("This is a simple example of the Mantle framework.");
-                    x.SetDisplayName("Mantle Simple Example Service");
-                    x.SetServiceName("MantleSimpleExampleService");
-                });
-        }
-
-        private static IWorker CreateWorker()
-        {
             var kernel = new StandardKernel();
 
             kernel.Load(Assembly.GetExecutingAssembly());
 
-            var worker = kernel.Get<IWorker>();
+            var host = new WindowsServiceWorkerHost(new NinjectDependencyResolver(kernel));
 
-            worker.ErrorOccurred += m => Trace.TraceError(m);
-            worker.MessageOccurred += m => Trace.TraceInformation(m);
-
-            return worker;
+            host.Start();
         }
     }
 }
