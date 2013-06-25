5d7bbdd1bcea185539d97bc2c70c69b7804df05a
Casey Watson <casey.j.watson@gmail.com>
2013-06-24 21:20:59 -0500

    Added sample console host.

---
+++ Mantle/Mantle.Sample.AddressBook.Storage.ConsoleHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Mantle.Console.Hosting;
+using Mantle.Ninject;
+using Ninject;
+
+namespace Mantle.Sample.AddressBook.Storage.ConsoleHost
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            var host = new ConsoleWorkerHost(new NinjectDependencyResolver(kernel));
+            host.Start();
+        }
+    }
+}
