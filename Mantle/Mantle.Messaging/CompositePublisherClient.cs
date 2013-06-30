93c6271605505a9ccc596d05468177ab5b3cc292
Casey Watson <casey.j.watson@gmail.com>
2013-06-30 15:38:49 -0500

    Added support for composite message publishing.

---
+++ Mantle/Mantle.Messaging/CompositePublisherClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mantle.Messaging
+{
+    public class CompositePublisherClient : IPublisherClient
+    {
+        private readonly List<IPublisherClient> publisherClients;
+
+        public CompositePublisherClient(CompositePublisherEndpoint endpoint)
+        {
+            if (endpoint == null)
+                throw new ArgumentNullException("endpoint");
+
+            endpoint.Validate();
+
+            publisherClients = new List<IPublisherClient>();
+
+            foreach (string endpointKey in endpoint.ChildEndpoints.Keys)
+                publisherClients.Add(endpoint.ChildEndpoints[endpointKey].GetClient());
+        }
+
+        public void Publish<T>(T message)
+        {
+            foreach (IPublisherClient publisherClient in publisherClients)
+                publisherClient.Publish(message);
+        }
+    }
+}
---
+++ Mantle/Mantle.Messaging/CompositePublisherEndpoint.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mantle.Messaging
+{
+    public class CompositePublisherEndpoint : Endpoint, IPublisherEndpoint
+    {
+        private readonly IPublisherEndpointDirectory publisherDirectory;
+
+        public CompositePublisherEndpoint(IPublisherEndpointDirectory publisherDirectory)
+        {
+            this.publisherDirectory = publisherDirectory;
+            ChildEndpoints = new Dictionary<string, IPublisherEndpoint>();
+        }
+
+        public Dictionary<string, IPublisherEndpoint> ChildEndpoints { get; set; }
+
+        public IPublisherClient GetClient()
+        {
+            return new CompositePublisherClient(this);
+        }
+
+        public override void Validate()
+        {
+            base.Validate();
+
+            if (ChildEndpoints.Count == 0)
+                throw new MessagingException(
+                    "Composite publisher endpoint must contain at least one child endpoint.");
+
+            foreach (string endpointKey in ChildEndpoints.Keys)
+                ChildEndpoints[endpointKey].Validate();
+        }
+
+        public void Setup(string name, params string[] childEndpointNames)
+        {
+            Name = name;
+
+            if ((childEndpointNames == null) || (childEndpointNames.Length == 0))
+                throw new MessagingException("At least one child endpoint name is required.");
+
+            foreach (string endpointName in childEndpointNames)
+            {
+                if (publisherDirectory.ContainsKey(endpointName) == false)
+                    throw new MessagingException(String.Format("The specified child endpoint [{0}] was not found.",
+                        endpointName));
+
+                if (ChildEndpoints.ContainsKey(endpointName))
+                    throw new MessagingException(
+                        String.Format("The specified child endpoint [{0}] has already been added.", endpointName));
+
+                ChildEndpoints.Add(endpointName, publisherDirectory[endpointName]);
+            }
+
+            Validate();
+        }
+    }
+}
