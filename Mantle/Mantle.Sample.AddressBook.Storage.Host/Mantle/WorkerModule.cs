9546f22fdea77303885a205435b0adce833644b3
Casey Watson <casey.j.watson@gmail.com>
2013-07-09 14:37:50 -0500

    Added Azure role worker host template. Added console and Azure role host Nuget packages.

---
+++ Mantle/Mantle.Sample.AddressBook.Storage.Host/Mantle/WorkerModule.cs
@@ -0,0 +1,13 @@
+﻿using Ninject.Modules;
+
+namespace Mantle.Sample.AddressBook.Storage.Host.Mantle
+{
+    public class WorkerModule : NinjectModule
+    {
+        public override void Load()
+        {
+            // TODO: Replace with your own concrete worker implementation.
+            // Bind<IWorker>().To<Worker>().InSingletonScope();
+        }
+    }
+}
