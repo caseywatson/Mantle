b4b00e02ced524159ce8399d79b44b1b256dea91
Casey Watson <casey.j.watson@gmail.com>
2013-08-15 14:28:17 -0500

    Added profiles support.

---
+++ Mantle/Mantle.Ninject/AssemblyExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ninject.Modules;
+
+namespace Mantle.Ninject
+{
+    public static class AssemblyExtensions
+    {
+        public static IEnumerable<INinjectModule> GetModulesByProfile(Assembly sourceAssembly, string profileName)
+        {
+            if (sourceAssembly == null)
+                throw new ArgumentNullException("sourceAssembly");
+
+            if (String.IsNullOrEmpty(profileName) == false)
+                throw new ArgumentNullException("profileName");
+
+            return sourceAssembly
+                .GetExportedTypes()
+                .Where(t => (IsLoadableModule(t) && IsInProfile(t, profileName)))
+                .Select(t => (Activator.CreateInstance(t) as INinjectModule));
+        }
+
+        private static bool IsLoadableModule(Type type)
+        {
+            return typeof (INinjectModule).IsAssignableFrom(type)
+                   && !type.IsAbstract
+                   && !type.IsInterface
+                   && type.GetConstructor(Type.EmptyTypes) != null;
+        }
+
+        private static bool IsInProfile(Type type, string profileName)
+        {
+            return
+                type.GetCustomAttributes(typeof (ProfileAttribute), false)
+                    .OfType<ProfileAttribute>()
+                    .Any(pa => (pa.ProfileName == profileName));
+        }
+    }
+}
---
+++ Mantle/Mantle/ProfileAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mantle
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    public sealed class ProfileAttribute : Attribute
+    {
+        public ProfileAttribute(string profileName)
+        {
+            if (String.IsNullOrEmpty(profileName))
+                throw new ArgumentNullException("profileName");
+
+            ProfileName = profileName;
+        }
+
+        public string ProfileName { get; private set; }
+    }
+}
