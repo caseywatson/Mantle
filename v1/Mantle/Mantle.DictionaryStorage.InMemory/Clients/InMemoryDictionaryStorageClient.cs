81f08f645a7a0f08181a40ea69388a04180df5ea
Casey Watson <casey.j.watson@gmail.com>
2014-06-23 23:25:32 -0500

    Added in-memory dictionary storage client.

---
+++ v1/Mantle/Mantle.DictionaryStorage.InMemory/Clients/InMemoryDictionaryStorageClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Mantle.DictionaryStorage.Interfaces;
+using Mantle.Extensions;
+
+namespace Mantle.DictionaryStorage.InMemory.Clients
+{
+    public class InMemoryDictionaryStorageClient<T> : IDictionaryStorageClient<T>
+        where T : class, new()
+    {
+        private readonly Dictionary<string, Dictionary<string, T>> dictionary; 
+        private readonly ReaderWriterLockSlim dictionaryLock;
+
+        public InMemoryDictionaryStorageClient()
+        {
+            dictionaryLock = new ReaderWriterLockSlim();
+        }
+
+        public void DeleteEntity(string entityId, string partitionId)
+        {
+            entityId.Require("entityId");
+            partitionId.Require("partitionId");
+        }
+
+        public bool EntityExists(string entityId, string partitionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOrUpdateEntity(T entity, string entityId, string partitionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOrUpdateEntities(IEnumerable<T> entities, Func<T, string> entityIdSelector, Func<T, string> partitionIdSelector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T LoadEntity(string entityId, string partitionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> LoadAllEntities(string parititionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
