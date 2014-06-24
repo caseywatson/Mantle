5681b3646481739857567982bf1ce128531071b3
Casey Watson <casey.j.watson@gmail.com>
2014-06-23 23:26:42 -0500

    Or, you know, I thought I did.

--- v1/Mantle/Mantle.BlobStorage.FileSystem/Clients/FileSystemBlobStorageClient.cs
+++ v1/Mantle/Mantle.BlobStorage.FileSystem/Clients/FileSystemBlobStorageClient.cs
@@ -1,12 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Mantle.BlobStorage.Interfaces;
+using Mantle.Configuration.Attributes;
+using Mantle.Extensions;
 
 namespace Mantle.BlobStorage.FileSystem.Clients
 {
-    class FileSystemBlobStorageClient
+    public class FileSystemBlobStorageClient : IBlobStorageClient
     {
+        [Configurable(IsRequired = true)]
+        public string Path { get; set; }
+
+        public void DeleteBlob(string blobName)
+        {
+            blobName.Require("blobName");
+
+            if (Directory.Exists(Path) == false)
+                throw new InvalidOperationException(String.Format("Directory [{0}] does not exist.", Path));
+
+            string filePath = System.IO.Path.Combine(Path, blobName);
+
+            if (File.Exists(filePath) == false)
+                throw new InvalidOperationException(String.Format("File [{0}] does not exist.", filePath));
+
+            File.Delete(filePath);
+        }
+
+        public bool BlobExists(string blobName)
+        {
+            blobName.Require("blobName");
+
+            if (Directory.Exists(Path) == false)
+                return false;
+
+            return (File.Exists(System.IO.Path.Combine(Path, blobName)));
+        }
+
+        public void InsertOrUpdateBlob(Stream blob, string blobName)
+        {
+            blob.Require("blob");
+            blobName.Require("blobName");
+
+            if (Directory.Exists(Path) == false)
+                throw new InvalidOperationException(String.Format("Directory [{0}] does not exist.", Path));
+
+            if (blob.CanSeek)
+                blob.Position = 0;
+
+            using (FileStream fileStream = File.Create(System.IO.Path.Combine(Path, blobName)))
+                blob.CopyTo(fileStream);
+        }
+
+        public IEnumerable<string> ListBlobs()
+        {
+            if (Directory.Exists(Path) == false)
+                throw new InvalidOperationException(String.Format("Directory [{0}] does not exist.", Path));
+
+            return Directory.GetFiles(Path).Select(System.IO.Path.GetFileName);
+        }
+
+        public Stream LoadBlob(string blobName)
+        {
+            blobName.Require("blobName");
+
+            if (Directory.Exists(Path) == false)
+                throw new InvalidOperationException(String.Format("Directory [{0}] does not exist.", Path));
+
+            string filePath = System.IO.Path.Combine(Path, blobName);
+
+            if (File.Exists(filePath) == false)
+                throw new InvalidOperationException(String.Format("File [{0}] does not exist.", filePath));
+
+            return (new MemoryStream(File.ReadAllBytes(filePath)) {Position = 0});
+        }
     }
 }
--- v1/Mantle/Mantle.DictionaryStorage.InMemory/Clients/InMemoryDictionaryStorageClient.cs
+++ v1/Mantle/Mantle.DictionaryStorage.InMemory/Clients/InMemoryDictionaryStorageClient.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Mantle.DictionaryStorage.Interfaces;
 using Mantle.Extensions;
 
@@ -12,11 +9,12 @@ namespace Mantle.DictionaryStorage.InMemory.Clients
     public class InMemoryDictionaryStorageClient<T> : IDictionaryStorageClient<T>
         where T : class, new()
     {
-        private readonly Dictionary<string, Dictionary<string, T>> dictionary; 
+        private readonly Dictionary<string, Dictionary<string, T>> dictionary;
         private readonly ReaderWriterLockSlim dictionaryLock;
 
         public InMemoryDictionaryStorageClient()
         {
+            dictionary = new Dictionary<string, Dictionary<string, T>>();
             dictionaryLock = new ReaderWriterLockSlim();
         }
 
@@ -24,31 +22,134 @@ namespace Mantle.DictionaryStorage.InMemory.Clients
         {
             entityId.Require("entityId");
             partitionId.Require("partitionId");
+
+            try
+            {
+                dictionaryLock.EnterWriteLock();
+
+                if (dictionary.ContainsKey(partitionId) == false)
+                    throw new InvalidOperationException(String.Format("Partition [{0}] does not exist.", partitionId));
+
+                if (dictionary[partitionId].ContainsKey(entityId) == false)
+                    throw new InvalidOperationException(String.Format("Entity [{0}/{1}] does not exist.", partitionId,
+                        entityId));
+
+                dictionary[partitionId].Remove(entityId);
+            }
+            finally
+            {
+                dictionaryLock.ExitWriteLock();
+            }
         }
 
         public bool EntityExists(string entityId, string partitionId)
         {
-            throw new NotImplementedException();
+            entityId.Require("entityId");
+            partitionId.Require("partitionId");
+
+            try
+            {
+                dictionaryLock.EnterReadLock();
+
+                if (dictionary.ContainsKey(partitionId) == false)
+                    return false;
+
+                if (dictionary[partitionId].ContainsKey(entityId))
+                    return false;
+
+                return true;
+            }
+            finally
+            {
+                dictionaryLock.ExitReadLock();
+            }
         }
 
         public void InsertOrUpdateEntity(T entity, string entityId, string partitionId)
         {
-            throw new NotImplementedException();
+            entity.Require("entity");
+            entityId.Require("entityId");
+            partitionId.Require("partitionId");
+
+            try
+            {
+                dictionaryLock.EnterWriteLock();
+
+                if (dictionary.ContainsKey(partitionId) == false)
+                    dictionary[partitionId] = new Dictionary<string, T>();
+
+                dictionary[partitionId][entityId] = entity;
+            }
+            finally
+            {
+                dictionaryLock.ExitWriteLock();
+            }
         }
 
-        public void InsertOrUpdateEntities(IEnumerable<T> entities, Func<T, string> entityIdSelector, Func<T, string> partitionIdSelector)
+        public void InsertOrUpdateEntities(IEnumerable<T> entities, Func<T, string> entityIdSelector,
+            Func<T, string> partitionIdSelector)
         {
-            throw new NotImplementedException();
+            entities.Require("entities");
+            entityIdSelector.Require("entityIdSelector");
+            partitionIdSelector.Require("partitionIdSelector");
+
+            try
+            {
+                dictionaryLock.EnterWriteLock();
+
+                foreach (T entity in entities)
+                {
+                    string entityId = entityIdSelector(entity);
+                    string partitionId = partitionIdSelector(entity);
+
+                    if (dictionary.ContainsKey(partitionId) == false)
+                        dictionary[partitionId] = new Dictionary<string, T>();
+
+                    dictionary[partitionId][entityId] = entity;
+                }
+            }
+            finally
+            {
+                dictionaryLock.ExitWriteLock();
+            }
         }
 
         public T LoadEntity(string entityId, string partitionId)
         {
-            throw new NotImplementedException();
+            entityId.Require("entityId");
+            partitionId.Require("partitionId");
+
+            try
+            {
+                if (dictionary.ContainsKey(partitionId) == false)
+                    throw new InvalidOperationException(String.Format("Partition [{0}] does not exist.", partitionId));
+
+                if (dictionary[partitionId].ContainsKey(entityId))
+                    return dictionary[partitionId][entityId];
+
+                return null;
+            }
+            finally
+            {
+                dictionaryLock.ExitReadLock();
+            }
         }
 
-        public IEnumerable<T> LoadAllEntities(string parititionId)
+        public IEnumerable<T> LoadAllEntities(string partitionId)
         {
-            throw new NotImplementedException();
+            partitionId.Require("partitionId");
+
+            try
+            {
+                if (dictionary.ContainsKey(partitionId) == false)
+                    throw new InvalidOperationException(String.Format("Partition [{0}] does not exist.", partitionId));
+
+                return (dictionary[partitionId].Values);
+            }
+            finally
+            {
+                dictionaryLock.ExitReadLock();
+            }
         }
     }
 }
