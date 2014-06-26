190792b35cb9a1bb7030dff4e35a834273e2b188
Casey Watson <casey.j.watson@gmail.com>
2014-06-25 19:56:28 -0500

    Tests. Tests. Tests.

---
+++ v1/Mantle/Mantle.Tests/GenericExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Mantle.Extensions;
+using NUnit.Framework;
+
+namespace Mantle.Tests
+{
+    [TestFixture]
+    public class GenericExtensionsTests
+    {
+        private const string TestObjectJson = "{\"Name\":\"Value\"}";
+
+        public class TestObject
+        {
+            public string Name { get; set; }
+        }
+
+        [Test]
+        public void Should_convert_Json_to_object()
+        {
+            var obj = TestObjectJson.FromJson<TestObject>();
+
+            Assert.IsNotNull(obj);
+            Assert.AreEqual(obj.Name, "Value");
+        }
+
+        [Test]
+        public void Should_convert_object_to_Json()
+        {
+            var testObject = new TestObject {Name = "Value"};
+            string jsonObject = testObject.ToJson();
+
+            Assert.IsNotNull(jsonObject);
+            Assert.AreEqual(TestObjectJson, jsonObject);
+        }
+
+        [Test]
+        public void When_converting_Json_to_an_object_should_throw_exception_if_object_string_is_null()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => (default(string)).FromJson<TestObject>());
+
+            Assert.IsNotNull(ex);
+            Assert.AreEqual(ex.ParamName, "objectString");
+        }
+    }
+}
