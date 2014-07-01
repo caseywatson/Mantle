9ad4a73166c46754a7b6de273e3c26e8fbbfa9a3
Casey Watson <cwatson@drndata.com>
2014-07-01 15:01:24 -0500

    I'm on to something?

--- v1/Mantle/Mantle.Hosting/Workers/SubscriberWorker.cs
+++ v1/Mantle/Mantle.Hosting/Workers/SubscriberWorker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Mantle.Configuration.Attributes;
 using Mantle.Interfaces;
 using Mantle.Messaging;
@@ -10,10 +11,18 @@ namespace Mantle.Hosting.Workers
     public class SubscriberWorker : BaseWorker
     {
         private readonly IDependencyResolver dependencyResolver;
+        private readonly Dictionary<Type, List<Action<Message>>> messageHandlers;
+        private readonly Dictionary<string, Type> messageTypeTokens;
+        private readonly List<ITypeTokenProvider> typeTokenProviders;
 
         public SubscriberWorker(IDependencyResolver dependencyResolver)
         {
             this.dependencyResolver = dependencyResolver;
+
+            messageHandlers = new Dictionary<Type, List<Action<Message>>>();
+            messageTypeTokens = new Dictionary<string, Type>();
+
+            typeTokenProviders = dependencyResolver.GetAll<ITypeTokenProvider>().ToList();
         }
 
         [Configurable]
@@ -39,6 +48,39 @@ namespace Mantle.Hosting.Workers
         public override void Stop()
         {
             throw new NotImplementedException();
+        }
+
+        public virtual void SubscribeTo<T>(ISubscriptionConstraint<T> constraint = null, string subscriberName = null)
+        {
+            SetupMessageType<T>();
+
+            var subscription = new Subscription<T>();
+
+            subscription.Subscriber = ((subscriberName == null)
+                ? (dependencyResolver.Get<ISubscriber<T>>())
+                : (dependencyResolver.Get<ISubscriber<T>>(subscriberName)));
+
+            messageHandlers[typeof (T)].Add(m => HandleMessage(m, subscription));
+        }
+
+        private void SetupMessageType<T>()
+        {
+            var tType = typeof (T);
+
+            foreach (var typeTokenProvider in typeTokenProviders)
+            {
+                var typeToken = typeTokenProvider.GetTypeToken<T>();
+
+                if (typeToken != null)
+                    messageTypeTokens[typeToken] = tType;
+            }
+
+            if (messageHandlers.ContainsKey(tType) == false)
+                messageHandlers.Add(tType, new List<Action<Message>>());
+        }
+
+        private void HandleMessage<T>(Message message, Subscription<T> subscription)
+        {
         }
     }
 }
--- v1/Mantle/Mantle.Messaging/Subscription.cs
+++ v1/Mantle/Mantle.Messaging/Subscription.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Mantle.Messaging.Interfaces;
 
 namespace Mantle.Messaging
 {
     public class Subscription<T>
     {
-        public Func<T, bool> Condition { get; set; }
+        public ISubscriptionConstraint<T> Constraint { get; set; }
         public ISubscriber<T> Subscriber { get; set; }
     }
 }
