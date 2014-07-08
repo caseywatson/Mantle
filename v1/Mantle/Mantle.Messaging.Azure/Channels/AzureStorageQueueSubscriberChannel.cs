ccb4aa80638d3954e8aefba620e529357a277777
Casey Watson <cwatson@drndata.com>
2014-07-08 17:09:43 -0500

    Build storage queue subscriber channel.

--- v1/Mantle/Mantle.Messaging.Azure/Channels/AzureStorageQueueSubscriberChannel.cs
+++ v1/Mantle/Mantle.Messaging.Azure/Channels/AzureStorageQueueSubscriberChannel.cs
@@ -1,25 +1,37 @@
 ﻿using System;
 using Mantle.Configuration.Attributes;
+using Mantle.Interfaces;
+using Mantle.Messaging.Azure.Context;
 using Mantle.Messaging.Interfaces;
 
 namespace Mantle.Messaging.Azure.Channels
 {
-    public class AzureStorageQueueSubscriberChannel<T> : ISubscriberChannel<T>
+    public class AzureStorageQueueSubscriberChannel<T> : BaseAzureStorageQueueChannel<T>, ISubscriberChannel<T>
     {
-
+        public AzureStorageQueueSubscriberChannel(ISerializer<T> serializer)
+            : base(serializer)
+        {
+        }
 
         [Configurable]
-        public bool AutoSetup { get; set; }
+        public override bool AutoSetup { get; set; }
 
         [Configurable(IsRequired = true)]
-        public string StorageConnectionString { get; set; }
+        public override string StorageConnectionString { get; set; }
 
         [Configurable(IsRequired = true)]
-        public string QueueName { get; set; }
+        public override string QueueName { get; set; }
 
         public IMessageContext<T> Receive(TimeSpan? timeout = null)
         {
-            
+            var message = ((timeout.HasValue)
+                ? (CloudQueue.GetMessage(timeout))
+                : (CloudQueue.GetMessage()));
+
+            if (message == null)
+                return null;
+
+            return new AzureCloudQueueMessageContext<T>(CloudQueue, message, Serializer.Deserialize(message.AsString));
         }
     }
 }
