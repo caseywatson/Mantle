d9c32f363729965e0bf34e94c43e234430f4a179
Casey Watson <cwatson@drndata.com>
2014-07-09 14:56:21 -0500

    Added console host.

---
+++ v1/Mantle/Mantle.Hosting.Console/ConsoleWorkerHost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Mantle.Hosting.Hosts;
+using Mantle.Hosting.Interfaces;
+
+namespace Mantle.Hosting.Console
+{
+    public class ConsoleWorkerHost : BaseWorkerHost
+    {
+        public ConsoleWorkerHost(IWorker worker)
+            : base(worker)
+        {
+        }
+
+        protected override void OnErrorOccurred(string message)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.Error.WriteLine(message);
+            System.Console.Error.WriteLine();
+        }
+
+        protected override void OnMessageOccurred(string message)
+        {
+            System.Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine(message);
+            System.Console.WriteLine();
+        }
+    }
+}
