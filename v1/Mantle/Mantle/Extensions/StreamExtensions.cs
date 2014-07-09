62f1363f62c70b453d592b16131d7197d3b52592
Casey Watson <cwatson@drndata.com>
2014-07-09 15:48:10 -0500

    Missed something.

--- v1/Mantle/Mantle/Extensions/StreamExtensions.cs
+++ v1/Mantle/Mantle/Extensions/StreamExtensions.cs
@@ -9,7 +9,12 @@ namespace Mantle.Extensions
             stream.Require("stream");
 
             if (stream.CanSeek)
+            {
                 stream.Position = 0;
+                return true;
+            }
+
+            return false;
         }
     }
 }
