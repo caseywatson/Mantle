adf308d723c7c31bc66a5d6383a7ff38ca99aac4
Casey Watson <cwatson@drndata.com>
2014-07-14 13:45:04 -0500

    Added multiplexing subscriber channel.

---
+++ v1/Mantle/Mantle.Messaging/Channels/MultiplexingSubscriberChannel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Mantle.Extensions;
+using Mantle.Messaging.Interfaces;
+
+namespace Mantle.Messaging.Channels
+{
+    public class MultiplexingSubscriberChannel<T> : ISubscriberChannel<T>
+        where T : class
+    {
+        private readonly ISubscriberChannel<T>[] childChannels;
+
+        public MultiplexingSubscriberChannel(params ISubscriberChannel<T>[] childChannels)
+        {
+            childChannels.Require("childChannels");
+            this.childChannels = childChannels;
+        }
+
+        public IMessageContext<T> Receive(TimeSpan? timeout = null)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var receiveAsyncTasksQuery =
+                childChannels.Select(cc => ReceiveAsync(cc, cancellationTokenSource.Token, timeout));
+
+            var receiveAsyncTasks = receiveAsyncTasksQuery.ToArray();
+            var firstFinishedTask = Task.WhenAny(receiveAsyncTasks).Result;
+
+            cancellationTokenSource.Cancel();
+
+            return firstFinishedTask.Result;
+        }
+
+        private async Task<IMessageContext<T>> ReceiveAsync(ISubscriberChannel<T> channel,
+                                                            CancellationToken cancellationToken, TimeSpan? timeout)
+        {
+            return await Task.Run(() => channel.Receive(timeout), cancellationToken);
+        }
+    }
+}
