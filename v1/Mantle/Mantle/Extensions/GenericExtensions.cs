f24342c0f5bf9b54f87780a039adde9be93c0b5f
Casey Watson <cwatson@drndata.com>
2014-07-17 11:14:47 -0500

    Flatten and inflate.

--- v1/Mantle/Mantle/Extensions/GenericExtensions.cs
+++ v1/Mantle/Mantle/Extensions/GenericExtensions.cs
@@ -1,14 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
 namespace Mantle.Extensions
 {
     public static class GenericExtensions
     {
+        public static Flattened<T> Flatten<T>(this T source)
+            where T : class
+        {
+            source.Require("source");
+
+            using (var sourceStream = new MemoryStream())
+            {
+                var serializer = new DataContractSerializer(typeof (T));
+
+                serializer.WriteObject(sourceStream, source);
+                sourceStream.TryToRewind();
+
+                return new Flattened<T>(sourceStream.ToArray());
+            }
+        }
+
         public static T FromJson<T>(this string objectString)
         {
             objectString.Require("objectString");
             return JsonConvert.DeserializeObject<T>(objectString);
+        }
+
+        public static T Inflate<T>(this Flattened<T> flattened)
+            where T : class
+        {
+            flattened.Require("flattened");
+
+            var serializer = new DataContractSerializer(typeof (T));
+            var sourceStream = new MemoryStream(flattened.Data);
+
+            sourceStream.TryToRewind();
+
+            return (serializer.ReadObject(sourceStream) as T);
         }
 
         public static void Require<T>(this T parameter, string parameterName, string errorMessage = null)
---
+++ v1/Mantle/Mantle/Flattened.cs
@@ -0,0 +1,19 @@
+﻿using Mantle.Extensions;
+
+namespace Mantle
+{
+    public class Flattened<T>
+    {
+        public Flattened()
+        {
+        }
+
+        public Flattened(byte[] data)
+        {
+            data.Require("data");
+            Data = data;
+        }
+
+        public byte[] Data { get; set; }
+    }
+}
