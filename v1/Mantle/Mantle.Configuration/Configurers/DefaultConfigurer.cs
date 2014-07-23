004a7e89e30fe4d0c06f415b50e551aa61a1d5d6
Casey Watson <casey.j.watson@gmail.com>
2014-07-22 20:23:00 -0500

    That was a lot less painful than I expected (that's what she said.)

---
+++ v1/Mantle/Mantle.Configuration/Configurers/DefaultConfigurer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Mantle.Configuration.Interfaces;
+using Mantle.Extensions;
+
+namespace Mantle.Configuration.Configurers
+{
+    public class DefaultConfigurer<T> : BaseConfigurer<T>
+    {
+        private readonly IConfigurationProvider configurationProvider;
+
+        public DefaultConfigurer(IConfigurationProvider configurationProvider)
+        {
+            configurationProvider.Require("configurationProvider");
+            this.configurationProvider = configurationProvider;
+        }
+
+        public override IEnumerable<ConfigurationSetting> GetConfigurationSettings()
+        {
+            return configurationProvider.GetConfigurationSettings();
+        }
+    }
+}
---
+++ v1/Mantle/Mantle.Configuration/Providers/CascadingConfigurationProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Mantle.Configuration.Interfaces;
+using Mantle.Extensions;
+
+namespace Mantle.Configuration.Providers
+{
+    public class CascadingConfigurationProvider : IConfigurationProvider
+    {
+        private readonly IConfigurationProvider[] childProviders;
+
+        public CascadingConfigurationProvider(params IConfigurationProvider[] childProviders)
+        {
+            childProviders.Require("childProviders");
+            this.childProviders = childProviders;
+        }
+
+        public IEnumerable<ConfigurationSetting> GetConfigurationSettings()
+        {
+            var configurationSettings = new Dictionary<string, ConfigurationSetting>();
+
+            foreach (IConfigurationProvider childProvider in childProviders)
+            {
+                foreach (ConfigurationSetting configurationSetting in childProvider.GetConfigurationSettings())
+                {
+                    configurationSettings[configurationSetting.Name] = configurationSetting;
+                }
+            }
+
+            return configurationSettings.Values;
+        }
+    }
+}
