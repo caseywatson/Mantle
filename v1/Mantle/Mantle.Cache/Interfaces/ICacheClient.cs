b439f12562201b94e60509ae40f31cec67321fc5
Casey Watson <casey.j.watson@gmail.com>
2014-10-15 20:10:05 -0500

    Doh

---
+++ v1/Mantle/Mantle.Cache/Interfaces/ICacheClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mantle.Cache.Interfaces
+{
+    public interface ICacheClient<T>
+        where T : class
+    {
+        T Get(string objectId);
+        void Put(T @object, string objectId, TimeSpan? cacheDuration = null);
+    }
+}
