f3becb8549fcea1519bca1b6d16dea6a140ec9c4
Casey Watson <casey.j.watson@gmail.com>
2016-05-17 18:47:29 -0500

    Where is this going?

---
+++ v1/Mantle/Mantle.Identity.Azure/Entities/DocumentDbMantleUserEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Mantle.Identity.Azure.Entities
+{
+    public class DocumentDbMantleUserEntity
+    {
+        [JsonProperty(PropertyName = "id")]
+        public virtual string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public virtual bool EmailConfirmed { get; set; }
+        public virtual bool LockoutEnabled { get; set; }
+        public virtual bool PhoneNumberConfirmed { get; set; }
+        public virtual bool TwoFactorEnabled { get; set; }
+
+        public virtual DateTimeOffset LockoutEndDate { get; set; }
+
+        public virtual int AccessFailedCount { get; set; }
+
+        public virtual List<MantleUserClaim> Claims { get; set; } = new List<MantleUserClaim>();
+        public virtual List<MantleUserLogin> Logins { get; set; } = new List<MantleUserLogin>();
+        public virtual List<string> Roles { get; set; } = new List<string>();
+
+        public virtual string Email { get; set; }
+        public virtual string PasswordHash { get; set; }
+        public virtual string PhoneNumber { get; set; }
+        public virtual string SecurityStamp { get; set; }
+        public virtual string UserName { get; set; }
+    }
+}
---
+++ v1/Mantle/Mantle.Identity.Azure/Repositories/DocumentDbMantleUserRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mantle.Identity.Interfaces;
+
+namespace Mantle.Identity.Azure.Repositories
+{
+    public class DocumentDbMantleUserRepository<T> : IMantleUserRepository<T>
+        where T : MantleUser
+    {
+        public void CreateUser(T user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateUserAsync(T user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindUserById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindUserByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindUserByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindUserByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(T user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(T user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
