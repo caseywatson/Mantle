4fd9b39e7bca4571e29dc13c299d9a7e68c2f9cb
Casey Watson <casey.j.watson@gmail.com>
2016-05-25 13:36:07 -0500

    Testing out LayeredDictionaryStorageClient bindings.

---
+++ v1/Mantle/Mantle.PhotoGallery.Web/Mantle/Profiles/AwsDeployment/DictionaryStorageModule.cs
@@ -0,0 +1,36 @@
+﻿using Mantle.Configuration.Configurers;
+using Mantle.DictionaryStorage.Aws.Clients;
+using Mantle.DictionaryStorage.Clients;
+using Mantle.DictionaryStorage.Interfaces;
+using Mantle.DictionaryStorage.Redis.Clients;
+using Mantle.Ninject;
+using Ninject.Modules;
+using Mantle.PhotoGallery.PhotoProcessing.Models;
+
+namespace Mantle.PhotoGallery.Web.Mantle.Profiles.AwsDeployment
+{
+    public class DictionaryStorageModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<RedisDictionaryStorageClient<PhotoMetadata>>()
+                .ToSelf()
+                .InTransientScope()
+                .ConfigureUsing(new ConnectionStringsConfigurer<RedisDictionaryStorageClient<PhotoMetadata>>(),
+                                new AppSettingsConfigurer<RedisDictionaryStorageClient<PhotoMetadata>>());
+
+            Bind<DynamoDbDictionaryStorageClient<PhotoMetadata>>()
+                .ToSelf()
+                .InTransientScope()
+                .ConfigureUsing(new ConnectionStringsConfigurer<DynamoDbDictionaryStorageClient<PhotoMetadata>>(),
+                                new AppSettingsConfigurer<DynamoDbDictionaryStorageClient<PhotoMetadata>>());
+
+            Bind<IDictionaryStorageClient<PhotoMetadata>>()
+                .To<LayeredDictionaryStorageClient<
+                    PhotoMetadata, 
+                    RedisDictionaryStorageClient<PhotoMetadata>,
+                    DynamoDbDictionaryStorageClient<PhotoMetadata>>>()
+                .InTransientScope();
+        }
+    }
+}
---
+++ v1/Mantle/Mantle.PhotoGallery.Web/Mantle/Profiles/AzureDeployment/DictionaryStorageModule.cs
@@ -0,0 +1,36 @@
+﻿using Mantle.Configuration.Configurers;
+using Mantle.DictionaryStorage.Azure.Clients;
+using Mantle.DictionaryStorage.Clients;
+using Mantle.DictionaryStorage.Interfaces;
+using Mantle.DictionaryStorage.Redis.Clients;
+using Mantle.Ninject;
+using Mantle.PhotoGallery.PhotoProcessing.Models;
+using Ninject.Modules;
+
+namespace Mantle.PhotoGallery.Web.Mantle.Profiles.AzureDeployment
+{
+    public class DictionaryStorageModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<RedisDictionaryStorageClient<PhotoMetadata>>()
+                .ToSelf()
+                .InTransientScope()
+                .ConfigureUsing(new ConnectionStringsConfigurer<RedisDictionaryStorageClient<PhotoMetadata>>(),
+                                new AppSettingsConfigurer<RedisDictionaryStorageClient<PhotoMetadata>>());
+
+            Bind<AzureTableDictionaryStorageClient<PhotoMetadata>>()
+                .ToSelf()
+                .InTransientScope()
+                .ConfigureUsing(new ConnectionStringsConfigurer<AzureTableDictionaryStorageClient<PhotoMetadata>>(),
+                                new AppSettingsConfigurer<AzureTableDictionaryStorageClient<PhotoMetadata>>());
+
+            Bind<IDictionaryStorageClient<PhotoMetadata>>()
+                .To<LayeredDictionaryStorageClient<
+                    PhotoMetadata,
+                    RedisDictionaryStorageClient<PhotoMetadata>,
+                    AzureTableDictionaryStorageClient<PhotoMetadata>>>()
+                .InTransientScope();
+        }
+    }
+}
