006b5da1053620a4438d5c8ef8afc0461f9710cb
Casey Watson <casey.j.watson@gmail.com>
2016-07-12 15:52:48 -0500

    Some kind of change.

--- v1/Mantle/Mantle.PhotoGallery.Web/Models/PhotoUploadViewModel.cs
+++ v1/Mantle/Mantle.PhotoGallery.Web/Models/PhotoUploadViewModel.cs
@@ -6,14 +6,14 @@ namespace Mantle.PhotoGallery.Web.Models
 {
     public class PhotoUploadViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
 
         public string Description { get; set; }
 
-        [MaxUploadSize((5*1024*1024), ErrorMessage = "Uploaded photo must be less than 5 MB in size.")]
-        [MustBeImage]
-        [Required]
+        [MaxUploadSize((5*1024*1024), ErrorMessage = "Photo must be 5 mb or less.")]
+        [MustBeImage(ErrorMessage = "Photo must be bmp, gif, jpg/jpeg or png.")]
+        [Required(ErrorMessage = "Photo is required.")]
         public HttpPostedFileBase Photo { get; set; }
     }
 }
